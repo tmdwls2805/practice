93beb5ebbf48be7055642dce39236328100e9072
Park <qkrtmdwls280@gmail.com>
2021-02-22 16:18:46 +0900

    Initial commit

---
+++ LSTM.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
